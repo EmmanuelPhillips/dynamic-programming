f0cd8e53ea02ec5265e4ae59fa458e4d50f48528
RammusOTP <emmanuelphillips33@gmail.com>
2026-08-05 23:15:18 +0100

    wrote min_coins function

--- coin_change.cpp
+++ coin_change.cpp
@@ -1,13 +1,33 @@
 #include <iostream>
 #include <vector>
 
-int min_coins(int change, const std::vector<int> &coins) { return 1; }
+int min_coins(int change, const std::vector<int> &coins) {
+  if (change < 0) {
+    return -1;
+  } // impossible to give change
+  if (change == 0) {
+    return 0;
+  } // no change needed
+  int overall_min{-1};
+  for (int coin : coins) {
+    int res{min_coins(change - coin, coins)};
+    if (res != -1) {
+      int current_min{res + 1};
+      if (overall_min == -1 || current_min < overall_min) {
+        overall_min = current_min;
+      }
+    }
+  }
+  return overall_min;
+}
 
 int main() {
-  int change{11};
-  std::vector<int> coin_options{1, 2, 5};
+  std::vector<int> coins{1, 2, 5};
+  int change{};
+  std::cout << "Enter change needed: ";
+  std::cin >> change;
 
-  std::cout << "Min coins needed: " << min_coins(change, coin_options);
+  std::cout << "Min coins needed: " << min_coins(change, coins) << '\n';
 
   return 0;
 }

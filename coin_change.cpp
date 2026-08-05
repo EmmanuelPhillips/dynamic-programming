2208374059b692048a84f3aff9b211c12b4f4eb6
RammusOTP <emmanuelphillips33@gmail.com>
2026-08-05 18:32:16 +0100

    made necessary template

--- coin_change.cpp
+++ coin_change.cpp
@@ -1,6 +1,13 @@
 #include <iostream>
+#include <vector>
+
+int min_coins(int change, const std::vector<int> &coins) { return 1; }
 
 int main() {
+  int change{11};
+  std::vector<int> coin_options{1, 2, 5};
 
-    return 0;
+  std::cout << "Min coins needed: " << min_coins(change, coin_options);
+
+  return 0;
 }

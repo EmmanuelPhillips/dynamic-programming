f5df6fe4ea1d928a3b5af8bd6e9dc4c0a05f7d1b
RammusOTP <emmanuelphillips33@gmail.com>
2026-08-11 13:11:16 +0100

    wrote code for knapsack.cpp

--- knapsack.cpp
+++ knapsack.cpp
@@ -1,7 +1,56 @@
 #include <iostream>
 #include <vector>
 
-int main() { return 0; }
+int knapsack(int capacity, std::vector<std::vector<int>> &items,
+             std::vector<std::vector<int>> &memo, int i, int j) {
+  if (i == items.size() || j == 0) {
+    return 0;
+  } // if no bag space, or no items left.
+
+  if (memo[i][j] != -1) {
+    return memo[i][j];
+  } // if alr known answer, then return that
+
+  int res{0};
+  if (items[i][0] <= j) {
+    int leave = knapsack(capacity, items, memo, i + 1, j);
+    int take =
+        items[i][1] + knapsack(capacity, items, memo, i + 1, j - items[i][0]);
+
+    res = std::max(leave, take);
+  } else {
+    res = knapsack(capacity, items, memo, i + 1, j);
+  }
+
+  memo[i][j] = res;
+  return res;
+}
+
+int main() {
+
+  int capacity{};
+  std::cout << "enter knapsack capacity: ";
+  std::cin >> capacity;
+
+  std::vector<std::vector<int>> items{
+      {2, 3}, // weight 2, value 3
+      {3, 4}, // weight 3, value 4
+      {4, 5}, // weight 4, value 5
+      {5, 8}, // weight 5, value 8
+      {9, 10} // weight 9, value 10
+  }; // [weight][value]
+
+  std::vector<std::vector<int>> memo(
+      items.size() + 1,
+      std::vector<int>(capacity + 1, -1)); // [value][capacity remaining]
+  int i{0u};
+  int j{0u};
+
+  std::cout << "max value with capacity " << capacity
+            << " is: " << knapsack(capacity, items, memo, i, j) << '\n';
+
+  return 0;
+}
 
 /* breaking it down "on paper"
  * - will have:

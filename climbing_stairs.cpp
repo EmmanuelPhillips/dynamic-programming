11f0aabefd72db140f1e1af76082002d88bcec1e
RammusOTP <emmanuelphillips33@gmail.com>
2026-08-13 00:37:01 +0100

    wrote code to see possible ways to climb stairs using 1 or 2 steps a time

---
+++ climbing_stairs.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <vector>
+
+int stairs(int num_stairs, std::vector<int> &memo) {
+  if (memo[num_stairs] != -1) {
+    return memo[num_stairs];
+  }
+  if (num_stairs <= 1) {
+    return 1;
+  } else {
+    memo[num_stairs] =
+        1 + stairs(num_stairs - 1, memo) + stairs(num_stairs - 2, memo);
+  }
+  return memo[num_stairs];
+}
+
+int main() {
+  int num_stairs{};
+  std::cout << "Enter number of stairs: ";
+  std::cin >> num_stairs;
+
+  std::vector<int> memo(num_stairs + 1, -1);
+
+  std::cout << "There are " << stairs(num_stairs, memo) << " ways to climb "
+            << num_stairs << " stairs.\n";
+  return 0;
+}

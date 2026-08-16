9dcb787ba65fdcc8eb78c2139b84eb2137ee9484
RammusOTP <emmanuelphillips33@gmail.com>
2026-08-16 21:05:16 +0100

    modified code to have vector defined inside of function (reduced scope for cleaner code)

--- fibonacci_tabulation.cpp
+++ fibonacci_tabulation.cpp
@@ -1,7 +1,10 @@
 #include <iostream>
 #include <vector>
 
-int fib(std::vector<int> &nums, int n) {
+int fib(int n) {
+  std::vector<int> nums(n + 1, -1);
+  nums[1] = 1;
+  nums[2] = 1;
   for (int i{3}; i <= n; ++i) {
     nums[i] = nums[i - 1] + nums[i - 2];
   }
@@ -13,11 +16,7 @@ int main() {
   std::cout << "Enter an index: ";
   std::cin >> n;
 
-  std::vector<int> nums(n + 1, -1);
-  nums[1] = 1;
-  nums[2] = 1;
-
-  std::cout << "fibonacci number at that index is: " << fib(nums, n) << '\n';
+  std::cout << "fibonacci number at that index is: " << fib(n) << '\n';
 
   return 0;
 }
